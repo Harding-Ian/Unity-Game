0124ee5aeef4b59d0eeb9e9bd9bd86ff63517f1b
Krafter54 <NathanC54@shaw.ca>
2024-03-28 21:14:31 -0600

    Movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Runtime.InteropServices;
-using System.Security.Cryptography;
-using System.Threading;
 using UnityEngine;
+using TMPro;
 using Unity.Netcode;
 using Unity.Collections;
 
 public class PlayerMovement : NetworkBehaviour
 {
+    [Header("Movement")]
+    public float moveSpeed;
 
+<<<<<<< Updated upstream
 
     [SerializeField] private Transform spawnedObjectPrefab;
     private NetworkVariable<MyCustomData> randomNumber = new NetworkVariable<MyCustomData>(
@@ -34,43 +34,55 @@ public class PlayerMovement : NetworkBehaviour
     }
 
     public CharacterController controller;
+=======
+    public float groundDrag;
+>>>>>>> Stashed changes
 
-    public float speed = 12f;
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMultiplier;
+    bool readyToJump;
 
-    public float gravity = -40f;
-    public float downwardSpeed = -0.05f;
-    public float dashSpeed = -12f;
+    [HideInInspector] public float walkSpeed;
+    [HideInInspector] public float sprintSpeed;
 
-    public Transform groundCheck;
-    public float groundRadius = 0.4f;
-    public LayerMask groundMask;
-    public float jumpHeight = 3f;
-    public float dashTimer = 0f;
+    [Header("Keybinds")]
+    public KeyCode jumpKey = KeyCode.Space;
 
-    Vector3 velocity;
-    Vector3 dashModifier;
-    Vector3 cameraDirection;
+    [Header("Ground Check")]
+    public float playerHeight;
+    public LayerMask whatIsGround;
+    bool grounded;
 
-    bool isGrounded;
-    bool isDashing;
-    bool canDash;
+    public Transform orientation;
 
+<<<<<<< Updated upstream
     public override void OnNetworkSpawn() {
         // randomNumber.OnValueChanged += (MyCustomData previousValue, MyCustomData newValue) => {
         //     Debug.Log(OwnerClientId + "; " + newValue._int + "; " + newValue._bool + " " + newValue.message);
         // };
     }
 
+=======
+    float horizontalInput;
+    float verticalInput;
+>>>>>>> Stashed changes
 
-    // Start is called before the first frame update
-    void Start()
+    Vector3 moveDirection;
+
+    Rigidbody rb;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+
+        readyToJump = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+<<<<<<< Updated upstream
         if (!IsOwner) return;
 
         // if (Input.GetMouseButtonDown(0)) {
@@ -102,15 +114,69 @@ public class PlayerMovement : NetworkBehaviour
         dashTimer += Time.deltaTime;
 
         if (dashTimer > 0.3)
-        {
-            isDashing = false;
-        }
-        
-        if (dashTimer > 2)
-        {
-            canDash = true;
-        }
+=======
+        // ground check
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.3f, whatIsGround);
 
+        MyInput();
+        SpeedControl();
+
+        // handle drag
+        if (grounded)
+            rb.drag = groundDrag;
+        else
+            rb.drag = 0;
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        // when to jump
+        if(Input.GetKey(jumpKey) && readyToJump && grounded)
+>>>>>>> Stashed changes
+        {
+            readyToJump = false;
+
+            Jump();
+
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
+    }
+
+    private void MovePlayer()
+    {
+        // calculate movement direction
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        // on ground
+        if(grounded)
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+
+        // in air
+        else if(!grounded)
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        // limit velocity if needed
+        if(flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
+    }
+
+<<<<<<< Updated upstream
         
         if (Input.GetKey(KeyCode.LeftShift) && canDash) //check for player attempting to dash
         {
@@ -170,3 +236,17 @@ public class PlayerMovement : NetworkBehaviour
         spawnedObjectTransform.GetComponent<NetworkObject>().Spawn(true);
      }
 }
+=======
+    private void Jump()
+    {
+        // reset y velocity
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+    private void ResetJump()
+    {
+        readyToJump = true;
+    }
+}
+>>>>>>> Stashed changes
