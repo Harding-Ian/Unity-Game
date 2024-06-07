23451762110e381a0611e1a23f8532d753ed5cf1
Yanmachine <hardingiant@gmail.com>
2024-06-07 00:58:25 -0600

    Blast Radius

--- Assets/Scripts/ProjectileBlast.cs
+++ Assets/Scripts/ProjectileBlast.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 
 public class ProjectileBlast : NetworkBehaviour
 {
+
+    public float radius = 4f;
+    public string playerTag = "Player";
     // Start is called before the first frame update
     void Start()
     {
         if (IsServer){
             Invoke("DestroyProjectile", 5);
+            FindPlayers();
         }
     }
 
@@ -23,5 +27,29 @@ public class ProjectileBlast : NetworkBehaviour
     {
         NetworkObject.Despawn();
     }
+
+    private void FindPlayers(){
+        Vector3 blastCenter = transform.position;
+
+        Collider[] hitColliders = Physics.OverlapSphere(blastCenter, radius);
+
+        List<GameObject> playersInRange = new List<GameObject>();
+
+        foreach (var hitCollider in hitColliders)
+        {
+            if (hitCollider.CompareTag(playerTag))
+            {
+                playersInRange.Add(hitCollider.gameObject);
+            }
+        }
+
+        Debug.Log("Players in range = " + playersInRange.Count);
+        int i = 0;
+        foreach (var x in playersInRange) {
+            Debug.Log(i + " = " + x.GetComponent<NetworkObject>().OwnerClientId);
+            i += 1;
+        }
+    }
+    
 
 }
